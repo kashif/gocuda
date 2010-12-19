0ef0ebfe850c0db193db6551b644cca7e731051e
Kashif Rasul <kashif@nomad-labs.com>
2010-12-19 16:31:52 +0100

    Added Device management.

--- cuda.go
+++ cuda.go
@@ -8,17 +8,39 @@ import "C"
 ** Initialization
 *********************************/
 
-func Init(flags C.uint) int {
-	return int(C.cuInit(flags))
+func Init(flags uint32) int {
+	return int(C.cuInit(C.uint(flags)))
 }
 
 /*********************************
 ** Driver Version Query
 *********************************/
 
-func DriverGetVersion(driverVersion *int) int {
-  var i C.int
-  ret := int(C.cuDriverGetVersion(&i))
-  *driverVersion = int(i)
-  return ret
+func DriverGetVersion() (driverVersion, err  int) {
+  var cDriverVersion, cErr C.int
+  cErr = C.int(C.cuDriverGetVersion(&cDriverVersion))
+  driverVersion = int(cDriverVersion)
+  err = int(cErr)
+  return
+}
+
+/************************************
+**    Device management
+***********************************/
+
+func DeviceGet(ordinal int) (device, err int) {
+	var cDevice C.CUdevice
+	var cErr C.int
+	cErr = C.int(C.cuDeviceGet(&cDevice, C.int(ordinal)))
+	device = int(cDevice)
+	err = int(cErr)
+	return
+}
+
+func DeviceGetCount() (count, err int) {
+	var cCount, cErr C.int
+	cErr = C.int(C.cuDeviceGetCount(&cCount))
+	count = int(cCount)
+	err = int(cErr)
+	return
 }
--- test.go
+++ test.go
@@ -6,10 +6,21 @@ import (
 )
 
 func main() {
-	res := cuda.Init(0)
-	fmt.Printf("%d\n", res)
+	err := cuda.Init(0)
+	fmt.Printf("Initialized: %d\n", err)
 	
-	driverVersion := 0
-	cuda.DriverGetVersion(&driverVersion)
-	fmt.Printf("%d\n", driverVersion)
+	driverVersion, err := cuda.DriverGetVersion()
+	if (err == 0) {
+		fmt.Printf("Driver Version: %d\n", driverVersion)
+	}
+	
+	device, err := cuda.DeviceGet(0)
+	if (err == 0) {
+		fmt.Printf("Device: %d\n", device)
+	}
+	
+	count, err := cuda.DeviceGetCount()
+	if (err == 0) {
+		fmt.Printf("Device count: %d\n", count)
+	}
 }
